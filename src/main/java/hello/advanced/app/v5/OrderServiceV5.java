079e8723ea3340fcec04d3268e086b1dc3f59c0d
dyllis <dev.dyllis@gmail.com>
2022-01-17 23:29:54 +0900

    20220117

--- src/main/java/hello/advanced/app/v5/OrderServiceV5.java
+++ src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -1,7 +1,7 @@
 package hello.advanced.app.v5;
 
 import hello.advanced.trace.callback.TraceTemplate;
-import hello.advanced.trace.template.AbstractTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,20 +10,16 @@ public class OrderServiceV5 {
     private final OrderRepositoryV5 orderRepository;
     private final TraceTemplate template;
 
-    public OrderServiceV5(OrderRepositoryV5 orderRepository) {
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
         this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
     }
 
     public void orderItem(String itemId) {
 
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
-                orderRepository.save(itemId);
-                return null;
-            }
-        };
-
-        template.execute("OrderService.orderItem()");
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
     }
 }
